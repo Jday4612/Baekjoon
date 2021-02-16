92baecefaa06079c0ad638affe16f2b95c18b364
Jday4612 <woo_jyu@kookmin.ac.kr>
2021-02-16 22:32:32 +0900

    A와 B의 나눗셈 출력

---
+++ Java/입출력과 사칙연산/A 나누기 B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+        Double a = s.nextDouble();
+        Double b = s.nextDouble();
+        System.out.print(a/b);
+	}
+}
