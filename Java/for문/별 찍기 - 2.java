7ed4b850b3c1d6e4f58bd3fc4062fee140f5e97c
Jday4612 <woo_jyu@kookmin.ac.kr>
2021-02-19 11:19:32 +0900

    1~N까지의 줄마다 1~N개의 별을 오른쪽 정렬로 출력

---
+++ Java/for문/별 찍기 - 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int k = Integer.parseInt(br.readLine());
+		for(int i = 1; i<k+1; i++) {
+			for(int j = 0; j<k-i; j++) {
+				bw.write(" ");
+			}
+			for(int t = 0; t<i; t++) {
+				bw.write("*");
+			}
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+    }
+}
