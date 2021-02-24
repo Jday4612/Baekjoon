45076362345762ec232706b4d14545839ce7ff7f
Jday4612 <woo_jyu@kookmin.ac.kr>
2021-02-24 17:45:22 +0900

    두 수가 주어졌을 때, 각각을  거꾸로 읽어 나온 수 중 가장 큰 수를 출력

---
+++ Java/문자열/상수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		System.out.print(a > b ? a : b);
+    }
+}
