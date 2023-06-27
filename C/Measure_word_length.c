7a34cd7aeaaed254cebd0ae3f86288f24842acae
WooJyu <66297198+Jday4612@users.noreply.github.com>
2023-06-28 00:02:13 +0900

    단어 길이 재기

---
+++ C/Measure_word_length.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char S[100];
+
+    scanf("%s", S);
+
+    printf("%d", (int)strlen(S));
+
+    return 0;
+}
