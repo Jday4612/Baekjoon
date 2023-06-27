40d3ae695300e6fad3f6e0f3323575c1f7c4e11d
WooJyu <66297198+Jday4612@users.noreply.github.com>
2023-06-28 01:28:53 +0900

    별 찍기 - 7

---
+++ C/star_7.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N - 1 - i; j++) {
+            printf(" ");
+        }
+        for (int k = 0; k < 2 * i + 1; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int i = N - 1; i > 0; i--) {
+        for (int j = 0; j < N - i; j++) {
+            printf(" ");
+        }
+        for (int k = 0; k < 2 * i - 1; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
